249229dbc10b981ed227db4945c9f6bb7feb53ac
lucas7234 <66056362+lucas7234@users.noreply.github.com>
2020-08-18 17:59:51 +0900

    Create MonthlyWage.c

---
+++ MonthlyWage.c
@@ -0,0 +1,14 @@
+//give wage to employees
+#include <stdio.h>
+#include <time.h>
+
+main()
+{
+  int employees;
+  printf("Number of employees?");
+  scanf(" %d", &employees);
+  int id[employees];
+  int wage[employees];
+  int date[employees];
+  int dateToday=tm_mday;
+}
