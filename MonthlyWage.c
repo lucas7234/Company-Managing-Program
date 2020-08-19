18dce3a19953770fe03d96bb5a676d96ad827d99
lucas7234 <66056362+lucas7234@users.noreply.github.com>
2020-08-19 09:37:52 +0900

    Update MonthlyWage.c

--- MonthlyWage.c
+++ MonthlyWage.c
@@ -1,6 +1,8 @@
 //give wage to employees
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+#include <stdlib.h>
 
 main()
 {
@@ -11,4 +13,34 @@ main()
   int wage[employees];
   int date[employees];
   int dateToday=tm_mday;
+  int temp
+  
+  //collect informations about wage
+  for (i=0; i==employees; i++)
+  {
+    printf("Enter employee's ID");
+    scanf(" %d", &id[i]);
+    printf("Wage of ID %d?", id[i]);
+    scanf(" %d", &wage[i]);
+    printf("ID %d should get his wage $%d at date ____?", id[i], wage[i]);
+    scanf(" %d", &date[i]);
+  }
+  
+  printf("When some employees should get wages, we will alert to you until you enter \'Y\'");
+  while (1)
+  {
+    dateToday=tm_mday;
+    temp=getch();
+    for (i=0; i==employees; i++)
+    {
+      if (dateToday==date[i])
+      {
+        while (temp!='Y')
+        {
+          printf("ID %d should get his/her wage! Give $%d to his/her!", id[i], wage[i]);
+        }
+      }
+    }
+  }
+  return 0;
 }
